d6925712a2a9c0d754c4f99a7a72ee13796f5810
theogenes <teocomp@gmail.com>
2018-11-16 12:37:02 -0200

    ajustes

--- app/Http/Controllers/AtendimentoController.php
+++ app/Http/Controllers/AtendimentoController.php
@@ -101,7 +101,7 @@ class AtendimentoController extends Controller
 	    			
 	    			if(is_null($ct_atendimento)) {
 	    				$ct_atendimento = new Atendimento();
-	    				$ct_atendimento->clinica_id = $atendimento["clinicaid"];
+	    				$ct_atendimento->clinica_id = $atendimento["clinica_id"];
 	    				$ct_atendimento->consulta_id = $consulta_id;
 	    				$ct_atendimento->ds_preco =  $atendimento["atendimentos"];
 	    				$ct_atendimento->cs_status = 'A';
@@ -245,7 +245,7 @@ class AtendimentoController extends Controller
     				$vigencia = $atendimento["data_inicio"].' - '.$atendimento["data_fim"];
     				$data_vigencia = UtilController::getDataRangeTimePickerToCarbon($vigencia);
     				dd($atendimento);
-    				$atendimento_id = $atendimento["id"];
+    				$atendimento_id = $atendimento["﻿id"];
     				//$atendimento = Atendimento::where(['clinica_id' => $atendimento['clinicaid'], 'consulta_id' => $consulta_id, 'cs_status' => 'A'])->first();
     				$ct_atendimento = Atendimento::findorfail($atendimento_id);
     	
@@ -253,9 +253,9 @@ class AtendimentoController extends Controller
     	
     				if(is_null($ct_atendimento)) {
     					$ct_atendimento = new Atendimento();
-    					$ct_atendimento->clinica_id = $atendimento["clinicaid"];
+    					$ct_atendimento->clinica_id = $atendimento["clinica_id"];
     					$ct_atendimento->consulta_id = $procedimento_id;
-    					$ct_atendimento->ds_preco =  $atendimento["atendimentos"];
+    					$ct_atendimento->ds_preco =  $atendimento["exames"];
     					$ct_atendimento->cs_status = 'A';
     					$ct_atendimento->save();
     				}
