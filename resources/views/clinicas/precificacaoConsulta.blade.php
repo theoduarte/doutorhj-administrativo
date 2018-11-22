1bacd8eb9100288e5d8fac8a626964f8b3238e69
theogenes <teocomp@gmail.com>
2018-11-22 16:28:25 -0200

    ajuste nome consultas

--- resources/views/clinicas/precificacaoConsulta.blade.php
+++ resources/views/clinicas/precificacaoConsulta.blade.php
@@ -88,7 +88,7 @@
     				<tr id="tr-{{$atendimento->id}}">
     					<td>{{$atendimento->id}} <input type="hidden" class="consulta_id" value="{{ $atendimento->consulta->id }}"> <input type="hidden" class="profissional_id" value="{{ $atendimento->profissional->id }}"></td>
     					<td><a href="{{ route('consultas.show', $atendimento->consulta->id) }}" title="Exibir" class="btn-link text-primary"><i class="ion-search"></i> {{$atendimento->consulta->cd_consulta}}</a></td>
-    					<td>{{$atendimento->ds_preco}}</td>
+    					<td class="text-left"><strong>Título geral: </strong><span class="text-primary">{{$atendimento->ds_preco}}</span><br><strong>Título específico: </strong><span class="text-danger">{{$atendimento->consulta->ds_consulta}}</span></td>
     					<td>@if($atendimento->profissional->cs_status == 'A') {{$atendimento->profissional->nm_primario.' '.$atendimento->profissional->nm_secundario.' ('.$atendimento->profissional->documentos()->first()->tp_documento.': '.$atendimento->profissional->documentos->first()->te_documento.')' }} @else <span class="text-danger"> <i class="mdi mdi-close-circle"></i> NENHUMA PROFISSIONAL SELECIONADO</span> @endif</td>
     					<td>@if( isset($atendimento->consulta->tag_populars) && sizeof($atendimento->consulta->tag_populars) > 0 ) <ul class="list-profissional-especialidade">@foreach($atendimento->consulta->tag_populars as $tag) <li><i class="mdi mdi-check"></i> {{ $tag->cs_tag }}</li> @endforeach</ul> @else <span class="text-danger"> <i class="ion-close-circled"></i></span>  @endif</td>
     					<td>
